892cbe0438012af68cf0d5259407b44961af0552
Deane Leung <deane.leung@gmail.com>
2023-01-16 17:02:48 +0800

    :white_check_mark: Add test for p1 and p3

--- src/p1.rs
+++ src/p1.rs
@@ -19,3 +19,23 @@ impl Solution {
         vec![]
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
+    }
+}
--- src/p3.rs
+++ src/p3.rs
@@ -7,18 +7,47 @@ impl Solution {
         let ss = s.split("").collect::<Vec<&str>>();
         let ss = &ss[1..ss.len() - 1];
         for v in ss {
-			if set.contains(v) {
-				if set.len() > res {
-					res = set.len();
-				}
-				let index = set.iter().position(|a| a == v).unwrap();
-				set = set.split_at(index + 1).1.to_vec();
-			}
-			set.insert(set.len(), v);
+            if set.contains(v) {
+                if set.len() > res {
+                    res = set.len();
+                }
+                let index = set.iter().position(|a| a == v).unwrap();
+                set = set.split_at(index + 1).1.to_vec();
+            }
+            set.insert(set.len(), v);
         }
         if set.len() > res {
             res = set.len();
         }
         res as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(
+            Solution::length_of_longest_substring("abcabcbb".to_string(),),
+            3
+        );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(
+            Solution::length_of_longest_substring("bbbbb".to_string(),),
+            1
+        );
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(
+            Solution::length_of_longest_substring("pwwkew".to_string(),),
+            3
+        );
     }
 }
