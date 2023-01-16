02a832bb027e650541e3c6236ef7e51ade0c316a
Deane Leung <deane.leung@gmail.com>
2023-01-16 16:32:30 +0800

    :sparkles: Add p9 solution

---
+++ src/p9.rs
@@ -0,0 +1,41 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+
+        if let Ok(x_rev) = x
+            .to_string()
+            .chars()
+            .rev()
+            .collect::<String>()
+            .parse::<i32>()
+        {
+            x == x_rev
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert!(Solution::is_palindrome(121));
+    }
+
+    #[test]
+    fn case2() {
+        assert!(!Solution::is_palindrome(-121));
+    }
+
+    #[test]
+    fn case3() {
+        assert!(!Solution::is_palindrome(10))
+    }
+}
